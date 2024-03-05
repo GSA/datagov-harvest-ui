d3d5efad7eabea51c5e3708f9e55c8652d4beafe
Tyler Burton <tyler.burton@gsa.gov>
2024-03-05 15:25:02 -0600

    adds conditionals to account for when a source has no jobs or an org has no sources; wires in search.gov to search bar

--- src/routes/organization/[orgId]/+page.svelte
+++ src/routes/organization/[orgId]/+page.svelte
@@ -18,56 +18,69 @@
         </div>
         <h2>Harvest Sources</h2>
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <div class="usa-table-container--scrollable" tabindex="0">
-            <table class="usa-table usa-table--striped">
-                <caption> Harvest Sources for Org Id: {data.organization.id} </caption>
-                <thead>
-                    <tr>
-                        <th data-sortable scope="col" role="columnheader">Health</th>
-                        <th data-sortable scope="col" role="columnheader">Name</th>
-                        <th data-sortable scope="col" role="columnheader">Url</th>
-                        <th data-sortable scope="col" role="columnheader">Id</th>
-                        <th data-sortable scope="col" role="columnheader">Notification Emails</th>
-                        <th data-sortable scope="col" role="columnheader">Organization Id</th>
-                        <th data-sortable scope="col" role="columnheader">Frequency</th>
-                        <th data-sortable scope="col" role="columnheader">Schema Type</th>
-                        <th data-sortable scope="col" role="columnheader">Source Type</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {#each data.harvest_sources as source}
+        {#if !data.harvest_sources.length}
+            No harvest sources found
+        {:else}
+            <div class="usa-table-container--scrollable" tabindex="0">
+                <table class="usa-table usa-table--striped">
+                    <caption> Harvest Sources for Org Id: {data.organization.id} </caption>
+                    <thead>
                         <tr>
-                            <th scope="row">
-                                <a
-                                    href="/organization/{data.params.orgId}/source/{source.id}/job/{data.harvest_jobs[
-                                        source.id
-                                    ][0].id}"
-                                >
-                                    <button
-                                        type="button"
-                                        class="usa-button usa-button{data.harvest_jobs[source.id][0].records_errored == 0
-                                            ? ''
-                                            : '--secondary'}"
-                                        >{data.harvest_jobs[source.id][0].records_errored == 0 ? 'Healthy' : 'Error'}</button
-                                    >
-                                </a>
-                            </th>
-                            <td data-sort-value={source.date_created}> {source.name}</td>
-                            <td data-sort-value={source.records_errored}><a href={source.url}>{source.url}</a></td>
-                            <td data-sort-value={source.id}
-                                ><a href="/organization/{data.params.orgId}/source/{source.id}">{source.id}</a></td
-                            >
-                            <td data-sort-value={source.date_finished}>{source.notification_emails} </td>
-                            <td data-sort-value={source.records_added}>{source.organization_id}</td>
-                            <td data-sort-value={source.records_deleted}>{source.frequency}</td>
-                            <td data-sort-value={source.records_ignored}>{source.schema_type}</td>
-                            <td data-sort-value={source.records_updated}>{source.source_type}</td>
+                            <th data-sortable scope="col" role="columnheader">Health</th>
+                            <th data-sortable scope="col" role="columnheader">Name</th>
+                            <th data-sortable scope="col" role="columnheader">Url</th>
+                            <th data-sortable scope="col" role="columnheader">Id</th>
+                            <th data-sortable scope="col" role="columnheader">Notification Emails</th>
+                            <th data-sortable scope="col" role="columnheader">Organization Id</th>
+                            <th data-sortable scope="col" role="columnheader">Frequency</th>
+                            <th data-sortable scope="col" role="columnheader">Schema Type</th>
+                            <th data-sortable scope="col" role="columnheader">Source Type</th>
                         </tr>
-                    {/each}
-                </tbody>
-            </table>
-            <div class="usa-sr-only usa-table__announcement-region" aria-live="polite"></div>
-        </div>
+                    </thead>
+                    <tbody>
+                        {#each data.harvest_sources as source, index}
+                            <tr>
+                                <th scope="row">
+                                    {#if data.harvest_jobs[source.id]}
+                                        <a
+                                            href="/organization/{data.params.orgId}/source/{source.id}/job/{data.harvest_jobs[
+                                                source.id
+                                            ][0].id}"
+                                        >
+                                            <button
+                                                type="button"
+                                                class="usa-button usa-button{data.harvest_jobs[source.id][index]
+                                                    .records_errored == 0
+                                                    ? ''
+                                                    : '--secondary'}"
+                                                >{data.harvest_jobs[source.id][index].records_errored == 0
+                                                    ? 'Healthy'
+                                                    : 'Error'}</button
+                                            >
+                                        </a>
+                                    {:else}
+                                        <button type="button" class="usa-button usa-button--base usa-button--hover">
+                                            N/A
+                                        </button>
+                                    {/if}
+                                </th>
+                                <td data-sort-value={source.date_created}
+                                    ><a href="/organization/{data.params.orgId}/source/{source.id}">{source.name}</a></td
+                                >
+                                <td data-sort-value={source.records_errored}><a href={source.url}>{source.url}</a></td>
+                                <td data-sort-value={source.id}>{source.id}</td>
+                                <td data-sort-value={source.date_finished}>{source.notification_emails} </td>
+                                <td data-sort-value={source.records_added}>{source.organization_id}</td>
+                                <td data-sort-value={source.records_deleted}>{source.frequency}</td>
+                                <td data-sort-value={source.records_ignored}>{source.schema_type}</td>
+                                <td data-sort-value={source.records_updated}>{source.source_type}</td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+                <div class="usa-sr-only usa-table__announcement-region" aria-live="polite"></div>
+            </div>
+        {/if}
     {/if}
 </div>
 
--- src/routes/organization/[orgId]/source/[sourceId]/+page.svelte
+++ src/routes/organization/[orgId]/source/[sourceId]/+page.svelte
@@ -30,58 +30,65 @@
             <p><b>Source Type:</b> {data.harvest_source.source_type}</p>
         </div>
         <h2>Harvest Jobs</h2>
-        <p>
-            <a href="/organization/{data.params.orgId}/source/{data.params.sourceId}/job/{data.harvest_jobs[0].id}"
-                >Last Job Status:</a
-            >
-            <svg
-                class="usa-icon align-middle {data.harvest_jobs[0].records_errored == 0 ? 'check' : 'cancel'}"
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-            >
-                <use xlink:href="/vendor/uswds/img/sprite.svg#{data.harvest_jobs[0].records_errored == 0 ? 'check' : 'cancel'}"
-                ></use>
-            </svg>
-        </p>
-        <Line data={data.chartData} />
-        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <div class="usa-table-container--scrollable" tabindex="0">
-            <table class="usa-table usa-table--striped">
-                <caption> Harvest Jobs for Harvest Source Id: {data.harvest_source.id} </caption>
-                <thead>
-                    <tr>
-                        <th data-sortable scope="col" role="columnheader">Id</th>
-                        <th data-sortable scope="col" role="columnheader">Date Created</th>
-                        <th data-sortable scope="col" role="columnheader">Date Finished</th>
-                        <th data-sortable scope="col" role="columnheader">Records Added</th>
-                        <th data-sortable scope="col" role="columnheader">Records Deleted</th>
-                        <th data-sortable scope="col" role="columnheader">Records Errored</th>
-                        <th data-sortable scope="col" role="columnheader">Records Ignored</th>
-                        <th data-sortable scope="col" role="columnheader">Records Updated</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {#each data.harvest_jobs as jobs}
+        {#if !data.harvest_jobs.length}
+            No harvest jobs found
+        {:else}
+            <p>
+                <a href="/organization/{data.params.orgId}/source/{data.params.sourceId}/job/{data.harvest_jobs[0].id}"
+                    >Last Job Status:</a
+                >
+                <svg
+                    class="usa-icon align-middle {data.harvest_jobs[0].records_errored == 0 ? 'check' : 'cancel'}"
+                    aria-hidden="true"
+                    focusable="false"
+                    role="img"
+                >
+                    <use
+                        xlink:href="/vendor/uswds/img/sprite.svg#{data.harvest_jobs[0].records_errored == 0
+                            ? 'check'
+                            : 'cancel'}"
+                    ></use>
+                </svg>
+            </p>
+            <Line data={data.chartData} />
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <div class="usa-table-container--scrollable" tabindex="0">
+                <table class="usa-table usa-table--striped">
+                    <caption> Harvest Jobs for Harvest Source Id: {data.harvest_source.id} </caption>
+                    <thead>
                         <tr>
-                            <th scope="row"
-                                ><a href="/organization/{data.params.orgId}/source/{data.params.sourceId}/job/{jobs.id}"
-                                    >{jobs.id}</a
-                                ></th
-                            >
-                            <td data-sort-value={jobs.date_created}> {jobs.date_created}</td>
-                            <td data-sort-value={jobs.date_finished}>{jobs.date_finished} </td>
-                            <td data-sort-value={jobs.records_added}>{jobs.records_added}</td>
-                            <td data-sort-value={jobs.records_deleted}>{jobs.records_deleted}</td>
-                            <td data-sort-value={jobs.records_errored}>{jobs.records_errored}</td>
-                            <td data-sort-value={jobs.records_ignored}>{jobs.records_ignored}</td>
-                            <td data-sort-value={jobs.records_updated}>{jobs.records_updated}</td>
+                            <th data-sortable scope="col" role="columnheader">Id</th>
+                            <th data-sortable scope="col" role="columnheader">Date Created</th>
+                            <th data-sortable scope="col" role="columnheader">Date Finished</th>
+                            <th data-sortable scope="col" role="columnheader">Records Added</th>
+                            <th data-sortable scope="col" role="columnheader">Records Deleted</th>
+                            <th data-sortable scope="col" role="columnheader">Records Errored</th>
+                            <th data-sortable scope="col" role="columnheader">Records Ignored</th>
+                            <th data-sortable scope="col" role="columnheader">Records Updated</th>
                         </tr>
-                    {/each}
-                </tbody>
-            </table>
-            <div class="usa-sr-only usa-table__announcement-region" aria-live="polite"></div>
-        </div>
+                    </thead>
+                    <tbody>
+                        {#each data.harvest_jobs as jobs}
+                            <tr>
+                                <th scope="row"
+                                    ><a href="/organization/{data.params.orgId}/source/{data.params.sourceId}/job/{jobs.id}"
+                                        >{jobs.id}</a
+                                    ></th
+                                >
+                                <td data-sort-value={jobs.date_created}> {jobs.date_created}</td>
+                                <td data-sort-value={jobs.date_finished}>{jobs.date_finished} </td>
+                                <td data-sort-value={jobs.records_added}>{jobs.records_added}</td>
+                                <td data-sort-value={jobs.records_deleted}>{jobs.records_deleted}</td>
+                                <td data-sort-value={jobs.records_errored}>{jobs.records_errored}</td>
+                                <td data-sort-value={jobs.records_ignored}>{jobs.records_ignored}</td>
+                                <td data-sort-value={jobs.records_updated}>{jobs.records_updated}</td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+                <div class="usa-sr-only usa-table__announcement-region" aria-live="polite"></div>
+            </div>
+        {/if}
     {/if}
 </div>
 
